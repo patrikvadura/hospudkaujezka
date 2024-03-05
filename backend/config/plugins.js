27f9adb42398f4dc3b4516e764a31c8497b26e94
Patrik Vaďura <patrikvadura@icloud.com>
2024-03-05 13:18:28 +0100

    strapi cloudinary

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -27,4 +27,20 @@ module.exports = ({ env }) => ({
       defaultReplyTo: env('SMTP_REPLYTO'),
     },
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        uploadStream: {
+          folder: env('CLOUDINARY_FOLDER', ''),
+        },
+        delete: {},
+      },
+    },
+  },
 });
