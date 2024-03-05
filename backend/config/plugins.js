fc9fad6cdc72b8c19a8d343fafca16ab2823f850
Patrik Vaďura <patrikvadura@icloud.com>
2024-03-05 20:12:50 +0100

    smtp

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -14,9 +14,9 @@ module.exports = ({ env }) => ({
   email: {
     provider: 'nodemailer',
     providerOptions: {
-      host: env('SMTP_HOST', 'smtp.gmail.com'),
+      host: env('SMTP_HOST', 'mail.hostliner.cz'),
       port: env('SMTP_PORT', 465),
-      secure: true,
+      secure: false,
       auth: {
         user: env('SMTP_USERNAME'),
         pass: env('SMTP_PASSWORD'),
